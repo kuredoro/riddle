0970f33c310935b81ffd473fe631bc7383e675db
CrazyDream1 <35272226+CrazyDream1@users.noreply.github.com>
2020-08-23 18:30:17 +0300

    Token class and enum (#3)
    * Token class and enum
    
    We need to check if all the tokens are in the enum.
    Do we need to add some staff in class Token?
    
    * Changes according to review
    
    * Rename Token.cpp to Token.hpp
    
    And change directories
    
    * Moved #pragma to the beginning of the file. Renamed file to follow naming conventions in the repo. Renamed code field in Token struct to type, since 'code' is misleading.
    
    * Pragma once
    
    Co-authored-by: しずえ <cauchymoon@mail.ru>
    Co-authored-by: しずえ <66309775+kureduro@users.noreply.github.com>

---
+++ lexer/token.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+
+enum class TokenType{
+    VarDec, // var
+    TypeDec, // type
+    RoutineDec, // routine
+    Is, // is
+    IntegerType, // integer
+    RealType, // real
+    BooleanType, // boolean
+    RecordType, // record
+    ArrayType, // array
+    True, // true
+    False, // false
+    WhileLoop, // while
+    ForLoop, // for
+    LoopBegin, // loop
+    End, // end
+    ReverseRange, // reverse
+    InRange, // in
+    If, // if
+    Else, // else
+    AndLogic, // and
+    OrLogic, // or
+    XorLogic, // xor
+    SmallerComp, // <
+    SeqComp, // <=
+    BiggerComp, // >
+    BeqComp, // >=
+    EqComp, // =
+    NeqComp, // /=
+    MultOp, // *
+    DevOp, // /
+    RemainderOp, // %
+    PlusOp, // +
+    MinusOp, // -
+    IntegerLiteral, // int const
+    RealLiteral, // real const
+    Identifier, // name
+    Dot, // .
+    TwoDots, // ..
+    Comma, // ,
+    BracketOpen, // (
+    BracketClose, // )
+    SquareBracketOpen, // [
+    SquareBracketClose // ]
+};
+
+struct Token{
+    TokenType type;
+    int srcPos;
+    std::string image;
+};
