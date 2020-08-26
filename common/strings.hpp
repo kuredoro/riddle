09f3068c9ad7b0549c81f7f8830422d142a0e5c1
しずえ <cauchymoon@mail.ru>
2020-08-26 12:00:54 +0300

    Added tokenization of keywords+identifiers and new lines. Reporting of position in the source.

---
+++ common/strings.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace common {
+
+
+std::string replaceAll(std::string str, const std::string& from, const std::string& to);
+
+
+} // namespace common
--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -5,24 +5,71 @@
 
 #include "trie.hpp"
 #include "token.hpp"
+#include <cctype>
 
 namespace lexer {
 
-//extern common::Trie<TokenType> trie;
+extern common::Trie<TokenType> g_keywordTrie;
 
 class Lexer {
 public:
-    Lexer(std::string src) : buf(src) {
-
-    }
+    Lexer(std::string src) : m_buf(src) {}
 
     Token Next() {
+
+        // Ignore spaces
+        for (; m_pos < m_buf.size() && Lexer::isSpace(m_buf[m_pos]); m_pos++) {}
+
+        if (m_pos == m_buf.size()) {
+            return { .type = TokenType::Eof };
+        }
+
+        // If alpha -> either a keyword or identifier -> read until space
+        if (std::isalpha(m_buf[m_pos])) {
+            int len = 1;
+            for (; m_pos + len < m_buf.size() && std::isalnum(m_buf[m_pos + len]); len++) {}
+
+            Token tok;
+
+            tok.image = m_buf.substr(m_pos, len);
+            tok.line = m_lineNum;
+            tok.srcPos = m_pos - m_lastAfterNewLine;
+            tok.type = g_keywordTrie.Find(tok.image).value_or(TokenType::Identifier);
+ 
+            m_pos += len;
+
+            return tok;
+        }
+
+        if (m_buf[m_pos] == '\n') {
+            Token tok;
+            tok.type = TokenType::NewLine;
+            tok.line = m_lineNum;
+            tok.srcPos = m_pos - m_lastAfterNewLine;
+            tok.image = "\n";
+
+            m_pos++;
+            m_lastAfterNewLine = m_pos;
+            m_lineNum++;
+
+            return tok;
+        }
+
+        // If not -> some operator, do maximal munch
+        
+        // Last failed? -> Error Token
+        
         return {};
     }
 
 private:
-    std::string buf;
-    
+    std::string m_buf;
+    size_t m_pos = 0, m_lastAfterNewLine = 0;
+    size_t m_lineNum = 1;
+
+    bool isSpace(char ch) {
+        return ch != '\n' && std::isspace(ch);
+    }
 };
 
 } // namespace lexer
