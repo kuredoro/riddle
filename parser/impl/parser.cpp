641797077e699b579deeeab7b99804159be6f8bd
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-09-07 21:46:00 +0300

    Add parser's barebone structure
    with markers for everyone to add their code without conflicts
    and some starter code

--- parser/impl/parser.cpp
+++ parser/impl/parser.cpp
@@ -1,5 +1,56 @@
 #include "parser.hpp"
+#include "token.hpp"
 
-void magic() {
-    ;
-}
+namespace AST
+{
+
+
+    // ---- @kureduro
+
+
+
+    // ---- @CrazyDream1
+
+    VariableNode Parser::parseVariable() {
+        // TODO: change to  .Peek() wherever checking for error ("expecting")
+        auto token = m_lexer.Next();
+        if (token.type != lexer::TokenType::Var) {
+            m_errors.push_back({
+                .position = token.position,
+                .message = "Expected \"var\" keyword but didn't find it.",
+            });
+        }
+        token = m_lexer.Next();
+        if (token.type != lexer::TokenType::Identifier) {
+            m_errors.push_back({
+                .position = token.position,
+                .message = "Expected an identifier after \"var\" keyword.",
+            });
+        }
+        // ...
+    }
+
+    // ---- @MefAldemisov
+
+    WhileLoopNode Parser::parseWhileLoop() {
+        // TODO: change to  .Peek() wherever checking for error ("expecting")
+        auto token = m_lexer.Next();
+        if (token.type != lexer::TokenType::While) {
+            m_errors.push_back({
+                .position = token.position,
+                .message = "Expected \"while\" keyword but didn't find it.",
+            });
+        }
+        // ...
+    }
+
+    // ---- @aabounegm
+
+    ExpressionNode Parser::parseExpression() {
+        // TODO: Concrete implementation
+    }
+
+    // ---- End separation
+
+
+} // namespace AST
--- parser/parser.hpp
+++ parser/parser.hpp
@@ -1,3 +1,72 @@
 #pragma once
 
-void magic();
+#include "structures.hpp"
+#include "lexer.hpp"
+
+
+namespace AST
+{
+
+
+struct Node {
+	position_t begin, end;
+};
+
+// TODO: fill the following structs with relevant data
+struct ProgramNode : Node {};
+struct RoutineNode : Node {};
+struct ParameterNode : Node {};
+struct TypeNode : Node {};
+struct PrimitiveTypeNode : TypeNode {};
+struct ArrayTypeNode : TypeNode {};
+struct RecordTypeNode : TypeNode {};
+struct VariableNode : Node {};
+struct BodyNode : Node {};
+struct StatementNode : Node {};
+struct AssignmentNode : StatementNode {};
+struct RoutineCallNode : StatementNode {};
+struct WhileLoopNode : StatementNode {};
+struct ForLoopNode : StatementNode {};
+struct IfStatementNode : StatementNode {};
+struct ExpressionNode : Node {};
+struct UnaryExpressionNode : ExpressionNode {};
+struct BinaryExpressionNode : ExpressionNode {};
+
+// TODO: define function to use this struct, that also advances
+struct Error {
+	position_t position;
+	std::string message;
+};
+
+// TODO: define a type for the "context"
+
+class Parser {
+public:
+	Parser(lexer::Lexer lexer) : m_lexer(lexer) {}
+	Node parseProgram();
+
+private:
+	lexer::Lexer m_lexer;
+	std::vector<Error> m_errors;
+
+	RoutineNode parseRoutine ();
+	ParameterNode parseParameter ();
+	TypeNode parseType ();
+	PrimitiveTypeNode parsePrimitiveType ();
+	ArrayTypeNode parseArrayType ();
+	RecordTypeNode parseRecordType ();
+	VariableNode parseVariable ();
+	BodyNode parseBody ();
+	StatementNode parseStatement ();
+	AssignmentNode parseAssignment ();
+	RoutineCallNode parseRoutineCall ();
+	WhileLoopNode parseWhileLoop ();
+	ForLoopNode parseForLoop ();
+	IfStatementNode parseIfStatement ();
+	ExpressionNode parseExpression ();
+	UnaryExpressionNode parseUnaryExpression ();
+	BinaryExpressionNode parseBinaryExpression ();
+};
+
+
+} // namespace AST
