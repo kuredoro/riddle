44e7a507a3e4a411d49daf478a11b1cefea949e2
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-09-09 19:17:14 +0300

    Add Visitor pattern implementation to nodes

--- demo/parser_demo.cpp
+++ demo/parser_demo.cpp
@@ -3,6 +3,62 @@
 #include "lexer.hpp"
 #include "parser.hpp"
 
+class PrintVisitor : public ast::Visitor {
+    void visit (ast::Program* node) override {
+
+    }
+    void visit(ast::Routine *node) override {
+
+    };
+    void visit(ast::Parameter *node) override {
+
+    };
+    void visit(ast::Type *node) override {
+
+    };
+    void visit(ast::PrimitiveType *node) override {
+
+    };
+    void visit(ast::ArrayType *node) override {
+
+    };
+    void visit(ast::RecordType *node) override {
+
+    };
+    void visit(ast::Variable *node) override {
+
+    };
+    void visit(ast::Body *node) override {
+
+    };
+    void visit(ast::Statement *node) override {
+
+    };
+    void visit(ast::Assignment *node) override {
+
+    };
+    void visit(ast::RoutineCall *node) override {
+
+    };
+    void visit(ast::WhileLoop *node) override {
+
+    };
+    void visit(ast::ForLoop *node) override {
+
+    };
+    void visit(ast::IfStatement *node) override {
+
+    };
+    void visit(ast::Expression *node) override {
+
+    };
+    void visit(ast::UnaryExpression *node) override {
+
+    };
+    void visit(ast::BinaryExpression *node) override {
+
+    };
+};
 
 int main (int argc, char *argv[]) {
 	if (argc == 2) {
@@ -11,10 +67,10 @@ int main (int argc, char *argv[]) {
                          (std::istreambuf_iterator<char>() ));
         fmt::print("Code:\n{}\n\n", code);
         lexer::Lexer lx{code};
-		AST::Parser parser(lx);
+		parser::Parser parser(lx);
         auto ast = parser.parseProgram();
-
-		// TODO: print the tree beautifully
+        PrintVisitor v;
+        ast->accept(v);
 
         return 0;
     }
