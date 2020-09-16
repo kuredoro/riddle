35036156e4a6908c211896cad919b59774041962
MefAldemisov <alisha-omg@yandex.ru>
2020-09-16 21:44:20 +0300

    first draft of tests added

--- tests/parser/parser_test.cpp
+++ tests/parser/parser_test.cpp
@@ -4,31 +4,36 @@
 #include "parser.hpp"
 #include "lexer.hpp"
 
-
 /**
  * Since only the parser should be tested here, a dummy (stub) lexer
  *  is used to not be affected by bugs in the actual parser
  */
-class DummyLexer : public lexer::Lexer {
+class DummyLexer : public lexer::Lexer
+{
 
 public:
-    DummyLexer(std::initializer_list<lexer::Token> tokens) : lexer::Lexer("") {
-        for (auto it = tokens.begin(); it != tokens.end(); it++) {
+    DummyLexer(std::initializer_list<lexer::Token> tokens) : lexer::Lexer("")
+    {
+        for (auto it = tokens.begin(); it != tokens.end(); it++)
+        {
             m_tokens.push_back(*it);
         }
         it = m_tokens.begin();
     }
 
-    lexer::Token Next() {
-        if (it == m_tokens.end()) {
-            return { .type = lexer::TokenType::Eof };
+    lexer::Token Next()
+    {
+        if (it == m_tokens.end())
+        {
+            return {.type = lexer::TokenType::Eof};
         }
         lexer::Token t = *it;
         it++;
         return t;
     }
 
-    lexer::Token operator[](size_t idx) {
+    lexer::Token operator[](size_t idx)
+    {
         return m_tokens.at(idx);
     }
 
@@ -37,9 +42,12 @@ private:
     std::vector<lexer::Token>::iterator it;
 };
 
-SCENARIO("Parser builds a tree from tokens") {
-    GIVEN("A lexer that gets tokens") {
-        WHEN("Tokens represent the expression '2+6/3'") {
+SCENARIO("Parser builds a tree from tokens")
+{
+    GIVEN("A lexer that gets tokens")
+    {
+        WHEN("Tokens represent the expression '2+6/3'")
+        {
             DummyLexer lx{
                 lexer::Token{
                     .type = lexer::TokenType::Int,
@@ -83,14 +91,16 @@ SCENARIO("Parser builds a tree from tokens") {
                 },
             };
 
-            THEN("Order of operations is preserved") {
+            THEN("Order of operations is preserved")
+            {
                 // parser::Parser parser(lx);
                 // auto tree = parser.parseExpression();
                 // TODO: compare the tree to the expected one
             }
         }
 
-        WHEN("Tokens represent a routine") {
+        WHEN("Tokens represent a routine")
+        {
             DummyLexer lx{
                 lexer::Token{
                     .type = lexer::TokenType::Routine,
@@ -111,19 +121,21 @@ SCENARIO("Parser builds a tree from tokens") {
                 // TODO
             };
 
-            THEN("It is parsed correctly") {
-                
+            THEN("It is parsed correctly")
+            {
             }
         }
 
-        WHEN("The routine is not named") {
+        WHEN("The routine is not named")
+        {
             // TODO
-            THEN("An error is reported") {
-
+            THEN("An error is reported")
+            {
             }
         }
 
-        WHEN("Tokens represent a parameter \"x: array integer\"") {
+        WHEN("Tokens represent a parameter \"x: array integer\"")
+        {
             DummyLexer lx{
                 lexer::Token{
                     .type = lexer::TokenType::Identifier,
@@ -167,7 +179,8 @@ SCENARIO("Parser builds a tree from tokens") {
             expectedType.elementType = std::make_shared<ast::PrimitiveType>(integer);
             expected.type = std::make_shared<ast::ArrayType>(expectedType);
 
-            THEN("It is parsed correctly") {
+            THEN("It is parsed correctly")
+            {
                 // parser::Parser parser(lx);
                 // auto tree = parser.parseParameter();
                 // CHECK((bool)tree);
@@ -175,7 +188,8 @@ SCENARIO("Parser builds a tree from tokens") {
             }
         }
 
-        WHEN("Tokens represent an array type \"array [ 5+3 ] real\"") {
+        WHEN("Tokens represent an array type \"array [ 5+3 ] real\"")
+        {
             DummyLexer lx{
                 lexer::Token{
                     .type = lexer::TokenType::Array,
@@ -237,7 +251,8 @@ SCENARIO("Parser builds a tree from tokens") {
 
             ast::ArrayType expected;
 
-            THEN("It is parsed correctly") {
+            THEN("It is parsed correctly")
+            {
                 // parser::Parser parser(lx);
                 // auto tree = parser.parseParameter();
                 // CHECK((bool)tree);
@@ -245,6 +260,197 @@ SCENARIO("Parser builds a tree from tokens") {
             }
         }
 
+        WHEN("Tokens represent a for loop with reverse")
+        {
+            DummyLexer lx{
+                lexer::Token{
+                    .type = lexer::TokenType::For,
+                    .pos = {
+                        .line = 1,
+                        .column = 1,
+                    },
+                    .lit = "for",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::NewLine,
+                    .pos = {
+                        .line = 1,
+                        .column = 5,
+                    },
+                    .lit = "i",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Identifier,
+                    .pos = {
+                        .line = 2,
+                        .column = 1,
+                    },
+                    .lit = "i",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::NewLine,
+                    .pos = {
+                        .line = 2,
+                        .column = 4,
+                    },
+                    .lit = "i",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::In,
+                    .pos = {
+                        .line = 3,
+                        .column = 1,
+                    },
+                    .lit = "in",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Reverse,
+                    .pos = {
+                        .line = 4,
+                        .column = 4,
+                    },
+                    .lit = "reverse",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Int,
+                    .pos = {
+                        .line = 4,
+                        .column = 4,
+                    },
+                    .lit = "5",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::TwoDots,
+                    .pos = {
+                        .line = 4,
+                        .column = 5,
+                    },
+                    .lit = "..",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Int,
+                    .pos = {
+                        .line = 4,
+                        .column = 7,
+                    },
+                    .lit = "10",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::NewLine,
+                    .pos = {
+                        .line = 4,
+                        .column = 9,
+                    },
+                    .lit = "\n",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Loop,
+                    .pos = {
+                        .line = 5,
+                        .column = 1,
+                    },
+                    .lit = "10",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::NewLine,
+                    .pos = {
+                        .line = 6,
+                        .column = 1,
+                    },
+                    .lit = "\n",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::End,
+                    .pos = {
+                        .line = 6,
+                        .column = 1,
+                    },
+                    .lit = "end",
+                },
+            };
 
+            THEN("It is parsed correctly")
+            {
+            }
+        }
+
+        WHEN("Tokens represent a while loop with reverse")
+        {
+            DummyLexer lx{
+                lexer::Token{
+                    .type = lexer::TokenType::Routine,
+                    .pos = {
+                        .line = 1,
+                        .column = 1,
+                    },
+                    .lit = "routine",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Identifier,
+                    .pos = {
+                        .line = 1,
+                        .column = 9,
+                    },
+                    .lit = "main",
+                },
+                // TODO
+            };
+
+            THEN("It is parsed correctly")
+            {
+            }
+        }
+        WHEN("Tokens represent an if-else statement")
+        {
+            DummyLexer lx{
+                lexer::Token{
+                    .type = lexer::TokenType::Routine,
+                    .pos = {
+                        .line = 1,
+                        .column = 1,
+                    },
+                    .lit = "routine",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Identifier,
+                    .pos = {
+                        .line = 1,
+                        .column = 9,
+                    },
+                    .lit = "main",
+                },
+                // TODO
+            };
+
+            THEN("It is parsed correctly")
+            {
+            }
+        }
+        WHEN("Tokens represent an if seatement without else")
+        {
+            DummyLexer lx{
+                lexer::Token{
+                    .type = lexer::TokenType::Routine,
+                    .pos = {
+                        .line = 1,
+                        .column = 1,
+                    },
+                    .lit = "routine",
+                },
+                lexer::Token{
+                    .type = lexer::TokenType::Identifier,
+                    .pos = {
+                        .line = 1,
+                        .column = 9,
+                    },
+                    .lit = "main",
+                },
+                // TODO
+            };
+
+            THEN("It is parsed correctly")
+            {
+            }
+        }
     }
 }
