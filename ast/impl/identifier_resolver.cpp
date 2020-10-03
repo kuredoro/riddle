5761052edff6aeeb8118cd5605ae00602714c15c
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-10-04 01:58:45 +0300

    WIP: implement IdentifierResolver visitor This visitor will resolve all Identifiers to either variable references  or RoutineCalls, and link them with the relevant declaration

---
+++ ast/impl/identifier_resolver.cpp
@@ -0,0 +1,227 @@
+#include "visitors.hpp"
+
+namespace visitors {
+
+void IdentifierResolver::visit(Program* node) {
+    // First, verify that no 2 routines have the same name
+    for (auto routine : node->routines) {
+        if (routines.find(routine->name) != routines.end()) {
+            // TODO: Move the following to a function/macro
+            m_errors.push_back(Error{
+                .pos = routine->begin,
+                .message = "A routine already exists with that name",
+            });
+            return;
+        }
+        routines.insert({routine->name, routine});
+    }
+
+    // Then, verify that global variables names are unique (from routines too)
+    for (auto variable : node->variables) {
+        // Any routine with the same name?
+        if (routines.find(variable->name) != routines.end()) {
+            m_errors.push_back(Error{
+                .pos = variable->begin,
+                .message = "A routine already exists with that name",
+            });
+            return;
+        }
+        // Any other global variable with the same name?
+        for (auto definedVariable : variables) {
+            if (definedVariable->name == variable->name) {
+                m_errors.push_back(Error{
+                    .pos = variable->begin,
+                    .message = "Another variable with the same name exists",
+                });
+                return;
+            }
+        }
+        variables.push_back(variable);
+    }
+
+    // TODO: do the same for types (since they can be given aliases)
+
+    // Finally, visit all routine declaration
+    for (auto routine : node->routines) {
+        routine->accept(*this);
+    }
+
+    variables.clear();
+    routines.clear();
+}
+
+void IdentifierResolver::visit(RoutineDecl* node) {
+    // To keep track of the scope, each function that introduces a new scope
+    //  will append its declarations to the `variables` vector and then remove
+    //  them again before returning.
+    // When searching for the closest variable declaration later on, the same
+    //  vector will be iterated on from end to start
+
+    auto oldSize = variables.size();
+
+    for (auto parameter : node->parameters) {
+        // TODO: decide if we should just remove the Parameter type and use
+        //  VariableDecl instead
+        // UPD: yes. do it.
+        auto param = std::make_shared<VariableDecl>();
+        param->name = parameter->name;
+        param->type = parameter->type;
+        param->begin = parameter->begin;
+        param->end = parameter->end;
+        variables.push_back(param);
+    }
+    node->body->accept(*this);
+
+    variables.resize(oldSize);
+}
+
+void IdentifierResolver::visit(Parameter* node) { node->type->accept(*this); }
+
+void IdentifierResolver::visit(Type*) {}
+
+void IdentifierResolver::visit(AliasedType* node) {
+    // TODO: find the type declaration that has the same name and link/replace
+}
+
+void IdentifierResolver::visit(PrimitiveType*) {}
+
+void IdentifierResolver::visit(IntegerType*) {}
+
+void IdentifierResolver::visit(RealType*) {}
+
+void IdentifierResolver::visit(BooleanType*) {}
+
+void IdentifierResolver::visit(ArrayType*) {}
+
+void IdentifierResolver::visit(RecordType* node) {
+    for (auto field : node->fields) {
+        field->accept(*this);
+    }
+}
+
+void IdentifierResolver::visit(VariableDecl* node) {
+    node->type->accept(*this);
+    if (node->expression != nullptr) {
+        node->expression->accept(*this);
+    }
+}
+
+void IdentifierResolver::visit(TypeDecl* node) { node->type->accept(*this); }
+
+void IdentifierResolver::visit(Body* node) {
+    auto oldSize = variables.size();
+
+    for (auto variable : node->variables) {
+        variables.push_back(variable);
+    }
+
+    for (auto statement : node->statements) {
+        statement->accept(*this);
+    }
+
+    variables.resize(oldSize);
+}
+
+void IdentifierResolver::visit(Statement*) {}
+
+void IdentifierResolver::visit(ReturnStatement* node) {
+    node->expression->accept(*this);
+}
+
+void IdentifierResolver::visit(Assignment* node) {
+    node->lhs->accept(*this);
+    node->rhs->accept(*this);
+}
+
+void IdentifierResolver::visit(WhileLoop* node) {
+    node->condition->accept(*this);
+    node->body->accept(*this);
+}
+
+void IdentifierResolver::visit(ForLoop* node) {
+    auto oldSize = variables.size();
+
+    variables.push_back(node->loopVar);
+
+    node->body->accept(*this);
+
+    variables.resize(oldSize);
+}
+
+void IdentifierResolver::visit(IfStatement* node) {
+    node->condition->accept(*this);
+    node->ifBody->accept(*this);
+    if (node->elseBody != nullptr) {
+        node->elseBody->accept(*this);
+    }
+}
+
+void IdentifierResolver::visit(Expression*) {}
+
+void IdentifierResolver::visit(UnaryExpression* node) {
+    node->operand->accept(*this);
+}
+
+void IdentifierResolver::visit(BinaryExpression* node) {
+    node->operand1->accept(*this);
+    node->operand2->accept(*this);
+}
+
+void IdentifierResolver::visit(Primary*) {}
+
+void IdentifierResolver::visit(IntegerLiteral*) {}
+
+void IdentifierResolver::visit(RealLiteral*) {}
+
+void IdentifierResolver::visit(BooleanLiteral*) {}
+
+void IdentifierResolver::visit(Identifier* node) {
+    // Here comes the main work of figuring out which variable/routine this
+    //  identifier refers to
+
+    // TODO: abstract away to a private function
+    for (auto it = variables.rbegin(); it != variables.rend(); it++) {
+        auto variable = *it;
+        if (variable->name == node->name) {
+            node->variable = variable;
+            node->type = *variable->type;
+            return;
+        }
+    }
+
+    // If it did not match any variable, it must be a parameterless routine call
+
+    auto routineIt = routines.find(node->name);
+    if (routineIt == routines.end()) {
+        m_errors.push_back(Error{
+            .pos = node->begin,
+            .message = "Unknown identifier",
+        });
+        return;
+    }
+
+    auto prim = static_cast<ast::Primary*>(node);
+    ast::RoutineCall call;
+    call.begin = node->begin;
+    call.routine = routineIt->second;
+    call.routineName = node->name;
+    call.end = node->end;
+    *prim = call;
+}
+
+void IdentifierResolver::visit(RoutineCall* node) {
+    auto routineIt = routines.find(node->routineName);
+    if (routineIt == routines.end()) {
+        m_errors.push_back(Error{
+            .pos = node->begin,
+            .message = "There is no routine with this name",
+        });
+        return;
+    }
+    node->routine = routineIt->second;
+    for (auto arg : node->args) {
+        arg->accept(*this);
+    }
+}
+
+} // namespace visitors
--- ast/visitors.hpp
+++ ast/visitors.hpp
@@ -41,4 +41,51 @@ private:
     size_t depth;
 };
 
+/**
+ * This visitor performs resolution of variables/routine calls to their
+ *  declarations.
+ * First, it verifies that there are no conflicts in names in each scope.
+ * Then, each instance of Identifier is either filled with a reference to its
+ *  declaration or replaced with a routine call referring to its routine
+ *  declaration.
+ */
+class IdentifierResolver : public Visitor {
+public:
+    void visit(Program* node) override;
+    void visit(RoutineDecl* node) override;
+    void visit(Parameter* node) override;
+    void visit(Type* node) override;
+    void visit(AliasedType* node) override;
+    void visit(PrimitiveType* node) override;
+    void visit(IntegerType* node) override;
+    void visit(RealType* node) override;
+    void visit(BooleanType* node) override;
+    void visit(ArrayType* node) override;
+    void visit(RecordType* node) override;
+    void visit(VariableDecl* node) override;
+    void visit(TypeDecl* node) override;
+    void visit(Body* node) override;
+    void visit(Statement* node) override;
+    void visit(ReturnStatement* node) override;
+    void visit(Assignment* node) override;
+    void visit(WhileLoop* node) override;
+    void visit(ForLoop* node) override;
+    void visit(IfStatement* node) override;
+    void visit(Expression* node) override;
+    void visit(UnaryExpression* node) override;
+    void visit(BinaryExpression* node) override;
+    void visit(Primary* node) override;
+    void visit(IntegerLiteral* node) override;
+    void visit(RealLiteral* node) override;
+    void visit(BooleanLiteral* node) override;
+    void visit(Identifier* node) override;
+    void visit(RoutineCall* node) override;
+
+private:
+    // `routines` is a map since we cannot have 2 routines with the same name
+    std::map<std::string, sPtr<RoutineDecl>> routines;
+    // `variables` is a vector to allow repetition (in different scopes)
+    std::vector<sPtr<VariableDecl>> variables;
+};
+
 } // namespace visitors
