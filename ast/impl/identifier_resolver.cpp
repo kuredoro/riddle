1d4990ad34d2c26483e24790d82907ec7f862b6a
しずえ <66309775+kuredoro@users.noreply.github.com>
2020-10-05 19:50:56 +0300

    Added a lot of blank lines for better readibility.

--- ast/impl/identifier_resolver.cpp
+++ ast/impl/identifier_resolver.cpp
@@ -16,8 +16,10 @@ void IdentifierResolver::visit(Program* node) {
         if (isRedeclared(node, typeDecl)) {
             ok = false;
         }
+
         typeDecl->accept(*this);
         checkReplacementType(typeDecl->type);
+
         m_types.push_back(typeDecl);
     }
 
@@ -25,14 +27,17 @@ void IdentifierResolver::visit(Program* node) {
         if (isRedeclared(node, var)) {
             ok = false;
         }
+
         if (var->initialValue != nullptr) {
             var->initialValue->accept(*this);
             checkReplacementVar(var->initialValue);
         }
+
         if (var->type != nullptr) {
             var->type->accept(*this);
             checkReplacementType(var->type);
         }
+
         m_variables.push_back(var);
     }
 
@@ -40,6 +45,7 @@ void IdentifierResolver::visit(Program* node) {
         if (isRedeclared(node, routine)) {
             ok = false;
         }
+
         m_routines.insert({routine->name, routine});
     }
 
@@ -68,6 +74,7 @@ void IdentifierResolver::visit(RoutineDecl* node) {
     for (auto& parameter : node->parameters) {
         parameter->type->accept(*this);
         checkReplacementType(parameter->type);
+
         m_variables.push_back(parameter);
     }
 
@@ -107,6 +114,7 @@ void IdentifierResolver::visit(ArrayType* node) {
         node->length->accept(*this);
         checkReplacementVar(node->length);
     }
+
     node->elementType->accept(*this);
     checkReplacementType(node->elementType);
 }
@@ -115,11 +123,11 @@ void IdentifierResolver::visit(RecordType* node) {
     // ensure that no two fields have the same name
     std::set<std::string> fieldNames;
     for (auto field : node->fields) {
-        // auto priorDecl =
         if (fieldNames.find(field->name) != fieldNames.end()) {
             error(field->begin, "field name is already in use");
             return;
         }
+
         fieldNames.insert(field->name);
     }
 
@@ -133,6 +141,7 @@ void IdentifierResolver::visit(VariableDecl* node) {
         node->type->accept(*this);
         checkReplacementType(node->type);
     }
+
     if (node->initialValue != nullptr) {
         node->initialValue->accept(*this);
         checkReplacementVar(node->initialValue);
@@ -173,15 +182,18 @@ void IdentifierResolver::visit(Body* node) {
     for (auto& type : node->types) {
         type->accept(*this);
         checkReplacementType(type->type);
+
         m_types.push_back(type);
     }
 
     for (auto& variable : node->variables) {
         m_variables.push_back(variable);
+
         if (variable->initialValue != nullptr) {
             variable->initialValue->accept(*this);
             checkReplacementVar(variable->initialValue);
         }
+
         if (variable->type != nullptr) {
             variable->type->accept(*this);
             checkReplacementType(variable->type);
@@ -206,6 +218,7 @@ void IdentifierResolver::visit(ReturnStatement* node) {
 void IdentifierResolver::visit(Assignment* node) {
     node->lhs->accept(*this);
     checkReplacementVar(node->lhs);
+
     node->rhs->accept(*this);
     checkReplacementVar(node->rhs);
 }
@@ -213,6 +226,7 @@ void IdentifierResolver::visit(Assignment* node) {
 void IdentifierResolver::visit(WhileLoop* node) {
     node->condition->accept(*this);
     checkReplacementVar(node->condition);
+
     node->body->accept(*this);
 }
 
@@ -229,7 +243,9 @@ void IdentifierResolver::visit(ForLoop* node) {
 void IdentifierResolver::visit(IfStatement* node) {
     node->condition->accept(*this);
     checkReplacementVar(node->condition);
+
     node->ifBody->accept(*this);
+
     if (node->elseBody != nullptr) {
         node->elseBody->accept(*this);
     }
@@ -245,6 +261,7 @@ void IdentifierResolver::visit(UnaryExpression* node) {
 void IdentifierResolver::visit(BinaryExpression* node) {
     node->operand1->accept(*this);
     checkReplacementVar(node->operand1);
+
     if (node->operation == lexer::TokenType::Dot) {
         // The left operand can be an expression (ex: `a.b` in `a.b.c`), but the
         //  right one always has to be an Identifier (cannot have `x.3`).
@@ -320,9 +337,11 @@ void IdentifierResolver::visit(Identifier* node) {
     auto variable = findVarDecl(node->name);
     if (variable != nullptr) {
         node->variable = variable;
+
         if (variable->type != nullptr) {
             node->type = variable->type;
         }
+
         return;
     }
 
@@ -341,7 +360,9 @@ void IdentifierResolver::visit(RoutineCall* node) {
         error(node->begin, "undeclared routine: {}", node->routineName);
         return;
     }
+
     node->routine = routineIt->second;
+
     for (auto& arg : node->args) {
         arg->accept(*this);
         checkReplacementVar(arg);
@@ -377,6 +398,7 @@ sPtr<VariableDecl> IdentifierResolver::findVarDecl(std::string name) {
             return variable;
         }
     }
+
     return nullptr;
 }
 
