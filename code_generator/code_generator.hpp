e0ed014cfed615965efcfb1c22c31117f17a02ea
Abdelrahman Aly Abounegm <a.abounegm@innopolis.university>
2020-10-10 01:49:39 +0300

    Restore code generation ability

--- code_generator/code_generator.hpp
+++ code_generator/code_generator.hpp
@@ -18,34 +18,34 @@
 
 namespace cg {
 
-class CodeGenerator : public ast::Visitor<llvm::Value*> {
+class CodeGenerator : public ast::Visitor {
 public:
     CodeGenerator(std::string name = "anonymous") : m_builder(m_context) {
         m_module = std::make_unique<llvm::Module>(name, m_context);
     }
-    llvm::Value* visit(ast::Program* node) override;
-    llvm::Value* visit(ast::RoutineDecl* node) override;
-    llvm::Value* visit(ast::AliasedType* node) override;
-    llvm::Value* visit(ast::IntegerType* node) override;
-    llvm::Value* visit(ast::RealType* node) override;
-    llvm::Value* visit(ast::BooleanType* node) override;
-    llvm::Value* visit(ast::ArrayType* node) override;
-    llvm::Value* visit(ast::RecordType* node) override;
-    llvm::Value* visit(ast::VariableDecl* node) override;
-    llvm::Value* visit(ast::TypeDecl* node) override;
-    llvm::Value* visit(ast::Body* node) override;
-    llvm::Value* visit(ast::ReturnStatement* node) override;
-    llvm::Value* visit(ast::Assignment* node) override;
-    llvm::Value* visit(ast::WhileLoop* node) override;
-    llvm::Value* visit(ast::ForLoop* node) override;
-    llvm::Value* visit(ast::IfStatement* node) override;
-    llvm::Value* visit(ast::UnaryExpression* node) override;
-    llvm::Value* visit(ast::BinaryExpression* node) override;
-    llvm::Value* visit(ast::IntegerLiteral* node) override;
-    llvm::ConstantFP* visit(ast::RealLiteral* node) override;
-    llvm::Value* visit(ast::BooleanLiteral* node) override;
-    llvm::Value* visit(ast::Identifier* node) override;
-    llvm::Value* visit(ast::RoutineCall* node) override;
+    void visit(ast::Program* node) override;
+    void visit(ast::RoutineDecl* node) override;
+    void visit(ast::AliasedType* node) override;
+    void visit(ast::IntegerType* node) override;
+    void visit(ast::RealType* node) override;
+    void visit(ast::BooleanType* node) override;
+    void visit(ast::ArrayType* node) override;
+    void visit(ast::RecordType* node) override;
+    void visit(ast::VariableDecl* node) override;
+    void visit(ast::TypeDecl* node) override;
+    void visit(ast::Body* node) override;
+    void visit(ast::ReturnStatement* node) override;
+    void visit(ast::Assignment* node) override;
+    void visit(ast::WhileLoop* node) override;
+    void visit(ast::ForLoop* node) override;
+    void visit(ast::IfStatement* node) override;
+    void visit(ast::UnaryExpression* node) override;
+    void visit(ast::BinaryExpression* node) override;
+    void visit(ast::IntegerLiteral* node) override;
+    void visit(ast::RealLiteral* node) override;
+    void visit(ast::BooleanLiteral* node) override;
+    void visit(ast::Identifier* node) override;
+    void visit(ast::RoutineCall* node) override;
 
     void print() { m_module->print(llvm::errs(), nullptr); }
 
@@ -54,6 +54,13 @@ private:
     llvm::IRBuilder<> m_builder;
     std::unique_ptr<llvm::Module> m_module;
     std::map<std::string, llvm::Value*> m_namedValues;
+
+    // used for holding values that should be returned by functions
+    llvm::Value* tempVal = nullptr;
+    void extractTempVal(llvm::Value*& insertInto) {
+        insertInto = tempVal;
+        tempVal = nullptr;
+    }
 };
 
 } // namespace cg
